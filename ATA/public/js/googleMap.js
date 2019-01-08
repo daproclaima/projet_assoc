128616a07167c174cc28bd9e0aee3154c251dd6e
Sébastien <sebastien.nobour@gmail.com>
2019-01-08 12:30:04 +0100

    commit du 8 janvier 2019 12:27
    Ajout de la vue contact twig implémentant map google via API + son css,
     et le ContactController '.
    Dans contact.html.twig, le component/_test.html.twig est un copié collé de https://developers.google.com/maps/documentation/javascript/adding-a-google-map.
    J'ai tenté de factoriser celui ci en googleMap.html.twig, googleMap.js, googleMap.css mais cela n'est pas fonctionnel pour le moment.
    
    Sébastien

--- ATA/public/js/googleMap.js
+++ ATA/public/js/googleMap.js
@@ -4,7 +4,7 @@ function initMap() {
     var uluru = {lat: -25.344, lng: 131.036};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('googleMap'), {zoom: 4, center: uluru});
+        document.getElementById('map'), {zoom: 4, center: uluru});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
 }
---
+++ ATA/src/Controller/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SNITPRO
+ * Date: 08/01/2019
+ * Time: 10:26
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+
+
+}
