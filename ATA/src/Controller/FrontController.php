2e9b9da447d463538fcd08c7156336fab60202bc
Hocine BOUSSAID <boussaid-hocine@hotmail.fr>
2019-01-08 15:29:09 +0100

    Evenements

--- ATA/src/Controller/FrontController.php
+++ ATA/src/Controller/FrontController.php
@@ -8,8 +8,8 @@
 
 namespace App\Controller;
 
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
@@ -27,5 +27,16 @@ class FrontController extends AbstractController
     public function apropos()
     {
         return $this->render('front/apropos.html.twig');
+    }
+
+
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/evenement", name="front_evenement")
+     */
+    public function evenement()
+    {
+        return $this->render('front/evenement.html.twig');
     }
 }
