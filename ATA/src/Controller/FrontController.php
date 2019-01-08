b1d210c6a16badd11dcd1787cb200d000794fa6d
Sébastien <sebastien.nobour@gmail.com>
2019-01-08 01:23:28 +0100

    travail sur API google map dans page contact

--- ATA/src/Controller/FrontController.php
+++ ATA/src/Controller/FrontController.php
@@ -10,12 +10,30 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
 
+    /**
+     * Matches /blog exactly
+     *
+     * @Route("/accueil", name="accueil")
+     */
+
     public function index()
     {
         return $this->render('front/index.html.twig');
+    }
+
+
+    /**
+     * Matches /blog exactly
+     *
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('front/contact.html.twig');
     }
 }
