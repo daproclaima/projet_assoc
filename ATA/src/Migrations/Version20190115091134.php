c80ac865bc87230d3566e4350ec658c227d8d309
Sébastien <sebastien.nobour@gmail.com>
2019-01-15 17:35:08 +0100

    essais fait sur page contact au niveau de la map
    Sans succès, il faut passer par de l'ajax et utiliser Openstreetmap ou Mapbox car google Map nous limite à un usage geocoder/jour.
    Seb

--- ATA/src/Migrations/Version20190115091134.php
+++ ATA/src/Migrations/Version20190115091134.php
@@ -8,14 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190114095200 extends AbstractMigration
+final class Version20190115091134 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article CHANGE featured_image featured_image VARCHAR(180) DEFAULT NULL');
+        $this->addSql('ALTER TABLE membre ADD reset_token VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -23,6 +23,6 @@ final class Version20190114095200 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article CHANGE featured_image featured_image VARCHAR(180) NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE membre DROP reset_token');
     }
 }
