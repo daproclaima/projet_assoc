dce93c4fc4a98dfa95d8d774fa8125f1752c25d9
Sébastien <sebastien.nobour@gmail.com>
2019-01-18 10:47:23 +0100

    OK

--- ATA/public/js/googleMap.js
+++ ATA/public/js/googleMap.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
+    // Localisation
+    var wf3 = {lat: 48.819, lng: 1.981};
+    // Centrage de la map sur la localisation
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+        document.getElementById('map'), {zoom:25, center: wf3});
+    // Positionnement du marqueur de position sur la carte
+    var marker = new google.maps.Marker({position: wf3, map: map});
 }
